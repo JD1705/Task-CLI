dfd0f9ca4323b64d86c4f3e40f8342e3f003438c
jd1705 <soulbusterjd17@gmail.com>
2025-08-22 13:00:37 -0400

    feat & refact: added list functionality and deleted interactive process

--- app/app.py
+++ app/app.py
@@ -64,57 +64,13 @@ if len(sys.argv) > 1:
             print("Task updated successfully!")
         else:
             print("There's no file to update")
-
+    
+    elif command == "list":
+        with open(path, 'r') as file:
+            json_file = json.load(file)
+            for i in json_file: print(f"task id: {i["id"]}\ntask description: {i["desc"]}\n")
+            file.close()
 
 
 else:
-    run = True
-    print("Task Manager Initialized\n")
-
-    while run:
-        print("Choose an option from below:")
-        print("1. Create new file\n2. Access to a file\n3. Exit")
-        option = input("Select one option: ")
-
-        if option == "1":
-            name = input("name: ")
-        
-            task = input("\nWrite a task to be added: ")
-            json_file.append({
-                    "id":index,
-                    "desc":task,
-                    "status":"to-do",
-                    "createdAt":f"{dt.datetime.now(dt.timezone.utc)}",
-                    "updatedAt":f"{dt.datetime.now(dt.timezone.utc)}"
-                    })
-
-            with open(f"{name}.json", "w") as file:
-                file.writelines(json.dumps(json_file, indent=4))
-                print("Task added successfully!\n")
-                file.close()
-
-            while run:
-                print("Choose what to do now:\n1. Add new task\n2. Update existing task\n3. Delete a task\n4. List all tasks\n5. Exit")
-                option = input("Select an option or write exit to quit: ")
-
-                if option == "1":
-                    task = input("\nWrite a task to be added: ")
-
-                    json_file.append({
-                        "id":index,
-                        "desc":task,
-                        "status":"to-do",
-                        "createdAt":f"{dt.datetime.now(dt.timezone.utc)}",
-                        "updatedAt":f"{dt.datetime.now(dt.timezone.utc)}"
-                        })
-
-                    with open(f"{name}.json", "w") as file:
-                        file.writelines(json.dumps(json_file,  indent=4))
-                        file.close()
-            
-                elif option == "2":
-                    task_id = int(input("\nWrite the id of the task you want to update: "))
-                
-                    print(json_file[task_id-1]["desc"])
-
-
+    print("not enough arguments to execute this command")
