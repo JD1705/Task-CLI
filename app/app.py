6f9c54dc6c435608f7d11f5a4f4796cecaec9805
jd1705 <soulbusterjd17@gmail.com>
2025-08-27 10:41:08 -0400

    feat: implement list tasks with state 'to-do'

--- app/app.py
+++ app/app.py
@@ -143,5 +143,19 @@ if len(sys.argv) > 1:
         else:
             print("Task id not found")
 
+    elif command == "list-to-do" and len(sys.argv) > 1:
+        to_do_tasks = []
+
+        with open(path, "r") as file:
+                json_file = json.load(file)
+                file.close()
+
+        for i in json_file:
+            if i["status"] == "to-do":
+                to_do_tasks.append(i)
+        
+        for i in to_do_tasks:
+                print(f"task id: {i["id"]}\ntask description: {i["desc"]}\ntask status: {i["status"]}\ntask creation date: {i["createdAt"]}\ntask update date: {i["updatedAt"]}\n")
+
 else:
     print("not enough arguments to execute this command")
